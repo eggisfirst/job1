0d47c1ce14d3981417fa25d125f3985c05a9ece8
xxx <>
2018-08-02 17:58:33 +0800

    change

--- questionnaire/qusetion.js
+++ questionnaire/qusetion.js
@@ -1,10 +1,11 @@
 /*中英文切换*/
 var btn = document.getElementsByTagName('button')
-btn[0].onclick = function () {
+btn[1].onclick = function () {
   btn[0].className = 'Chinese';
   btn[1].className = ('English-on')
 }
-btn[1].onclick = function () {
+btn[0].onclick = function () {
   btn[0].className = 'Chinese-on'; 
   btn[1].className = ('English')
 }
+
