4aad0736a77aa081d18cc31c8ce54f1d1962b033
xxx <>
2018-08-02 12:38:07 +0800

    add

---
+++ questionnaire/qusetion.js
@@ -0,0 +1,9 @@
+
+
+
+var zw = document.getElementsByClassName('Chinese-on')
+var eng = document.getElementsByClassName('English')
+eng.onclick = function(){
+  zw.className = ('Chinese')
+  eng.className = ('English-on')
+}
